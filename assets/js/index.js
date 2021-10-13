64019d37557406d397026eb67d2891bc0500a2ee
KimLerbaekPedersen <kilepe@hotmail.com>
2021-10-13 13:29:38 +0200

    hello

--- assets/js/index.js
+++ assets/js/index.js
@@ -70,10 +70,6 @@ function autoResize(element, nativeSize) {
 }
 //======CLOCK END======//
 
-//======WEATHER API START======//
-!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
-//======WEATHER API END======//
-
 // SLIDESHOW START
 var slideIndex = 0;
 showSlides();
@@ -92,6 +88,6 @@ function showSlides() {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 5000);
+  setTimeout(showSlides,5000);
 }
 // SLIDESHOW END
